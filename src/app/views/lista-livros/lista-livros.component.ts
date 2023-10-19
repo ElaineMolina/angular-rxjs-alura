2dffab12a94f788129dec1a8f6df448f5dcee4a6
ElaineMolina <elainemolina10@gmail.com>
2023-10-19 16:58:15 -0300

    operadores rxjs

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,28 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
-
-  listaLivros: [];
-  campoBusca: string = "";
+export class ListaLivrosComponent implements OnDestroy{
+  listaLivros: Livro [];
+  campoBusca: string = '';
   // campoBusca = ""; //quando inicializa não precisa tipar
+  subscripton: Subscription;
+  livro: Livro;
 
-  constructor(
-    private service: LivroService
-  ) { }
+  constructor(private service: LivroService) {}
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => console.log(retornoAPI),
-      (error) => console.log(error)
-    )
+    this,
+      this.subscripton = this.service.buscar(this.campoBusca).subscribe({
+        next: (items) =>{
+          this.listaLivros = this.livrosResultadoParaLivros(items)
+        },
+        error: (erro) => console.log(erro),
+      })
+  }
+
+  livrosResultadoParaLivros(items) : Livro[]{
+    const livros :  Livro[] = [];
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    })
+    return livros;
+  }
+
+  ngOnDestroy() {
+    this.subscripton.unsubscribe();
   }
 }
-
-
-
